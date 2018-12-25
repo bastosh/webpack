95701d44ae72d224e5d5b38727d753df0ae44b8b
Sébastien Pereda <sebastien.pereda@gmail.com>
2018-12-25 16:50:57 +0100

    Organize files in src

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
+const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
+  entry: './src/js/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
   module: {
     rules: [
       {
